2f642e7addec655e75b3660d7fa48ab7b3de96a0
DHANESHJOSHI <hydradhanesh@gmail.com>
2024-04-27 09:03:58 +0530

    updated

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = "ws://15.206.153.253:3000"
 
 const SocketContext = createContext()
 
