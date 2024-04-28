d1c6d1ee06a8eae8a29fb1f4c6d0f954c833aae6
DHANESHJOSHI <hydradhanesh@gmail.com>
2024-04-28 09:10:22 +0530

    updated socket

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = "https://bug-free-space-enigma-6ww7v77w95qh9j-3000.app.github.dev/"
+const BACKEND_URL = "http://3.110.157.177:3000/"
 
 const SocketContext = createContext()
 
