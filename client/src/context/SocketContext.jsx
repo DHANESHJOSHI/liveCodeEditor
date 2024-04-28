cc3b5c4f4bb447a59b0b4d887e3f613867bfa618
DHANESHJOSHI <hydradhanesh@gmail.com>
2024-04-28 09:30:25 +0530

    without ssl

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = "https://3.110.157.177:3000/"
+const BACKEND_URL = "http://3.110.157.177:3000/"
 
 const SocketContext = createContext()
 
